9114cce1f0619d895c58c571aafbf8b340066194
shih wenfeng <ke265379ke@gmail.com>
2018-12-29 23:52:36 +0800

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,4 +1,4 @@
-def insertionSort2(n, arr):
+def insertionSort1(n, arr):
     for i in range(1,n):
         x=arr[i]
         j=i-1
@@ -6,3 +6,14 @@ def insertionSort2(n, arr):
             arr[j+1]=arr[j]
             j-=1
         arr[j+1]=x
+        
+def insertionSort2(n, arr):
+    if n==1: 
+        xx(arr)
+        print('')
+    for i in range(1,n):
+        for j in range(i,0,-1):
+            if arr[j]<arr[j-1]: 
+                (arr[j],arr[j-1])=(arr[j-1],arr[j])
+            else:
+                break
