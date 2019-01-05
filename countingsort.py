a125421364e7d589f924a13b382d1d8859e8a4ae
shih wenfeng <ke265379ke@gmail.com>
2019-01-05 16:02:24 +0800

    Update countingsort.py

--- countingsort.py
+++ countingsort.py
@@ -1,6 +1,14 @@
 #Count numbers between 0 and 100
 def countingSort(arr):
-    x=list(0 for x in range(100))
+    x=list(0 for x in range(max(arr)+1))
+    a=[]
     for i in arr:
         x[i]=x[i]+1
-    return (x)
+
+    for j in range(max(arr)+1):
+        if x[j]!=0:
+            for k in range(x[j]):
+                a.append(j)
+        else:
+            continue
+    return(a)
