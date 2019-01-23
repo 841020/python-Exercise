9aac92656b68a0d2008625093daec4bd159279db
shih wenfeng <ke265379ke@gmail.com>
2019-01-23 22:11:41 +0800

    Vs code automatic typesetting

--- countingsort.py
+++ countingsort.py
@@ -1,11 +1,11 @@
 def countingSort(arr):
-    x=list(0 for x in range(max(arr)+1))
-    a=[]
+    x = list(0 for x in range(max(arr)+1))
+    a = []
     for i in arr:
-        x[i]=x[i]+1
+        x[i] = x[i]+1
 
     for j in range(max(arr)+1):
-        if x[j]!=0:
+        if x[j] != 0:
             for k in range(x[j]):
                 a.append(j)
     return(a)
