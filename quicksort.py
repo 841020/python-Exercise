0373303d1cbf8d0d48a3ce56625ce1d854ec02da
shih wenfeng <ke265379ke@gmail.com>
2019-01-23 22:09:59 +0800

    vs code Automatic typesetting

--- quicksort.py
+++ quicksort.py
@@ -1,16 +1,19 @@
-#The most important thing about quicksort is Partition.
-#quicksort is unstable,Because at the time of Partition, the same size as the benchmark can go to either side.
-#this is only Partition
+# The most important thing about quicksort is Partition.
+# quicksort is unstable,Because at the time of Partition,
+# the same size as the benchmark can go to either side.
+# this is only Partition
+
+
 def quickSort(arr):
-    k=[]
-    kr=[]
-    kl=[]
+    k = []
+    kr = []
+    kl = []
     for i in range(len(arr)):
-        if i==0:
+        if i == 0:
             k.append(arr[i])
-        elif arr[i]>arr[0]:
+        elif arr[i] > arr[0]:
             kr.append(arr[i])
 
-        elif arr[i]<arr[0]:
+        elif arr[i] < arr[0]:
             kl.append(arr[i])
     return(kl+k+kr)
