b04c02a0d225908d3dba1fec2146dc7e8da63f80
shih wenfeng <ke265379ke@gmail.com>
2019-01-23 22:11:10 +0800

    Vs code automatic typesetting

--- insertionSort.py
+++ insertionSort.py
@@ -1,26 +1,30 @@
-#The most important thing about insert sorting is to build ordered arrays.
+# The most important thing about insert sorting is to build ordered arrays.
 def xx(y):
     for i in arr:
-        print(i,end=' ')
-        
-#From back to front        
+        print(i, end=' ')
+
+# From back to front
+
+
 def insertionSort2(n, arr):
-    if n==1: 
+    if n == 1:
         xx(arr)
         print('')
-    for i in range(1,n):
-        for j in range(i,0,-1):
-            if arr[j]<arr[j-1]: 
-                (arr[j],arr[j-1])=(arr[j-1],arr[j])
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                (arr[j], arr[j-1]) = (arr[j-1], arr[j])
             else:
                 break
         xx(arr)
-        print('') 
-#By going to
-def insertionsort(n,arr):
-    for i in range(1,n):
-        x=arr[i]
+        print('')
+# By going to
+
+
+def insertionsort(n, arr):
+    for i in range(1, n):
+        x = arr[i]
         for j in range(i):
-            if arr[j]>x:
-                (arr[i],arr[j])=(arr[j],arr[i])
-    return arr 
+            if arr[j] > x:
+                (arr[i], arr[j]) = (arr[j], arr[i])
+    return arr
