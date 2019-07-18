585206cc105c8e321091055fb773e8bb30276a11
mike <ke265379ke@gmail.com>
2019-07-18 21:19:07 +0800

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,27 +1,27 @@
 # Move the smallest number to the front
 def bubblesort1(a):
-    counter = 0
-    for i in range(len(a)-1):
+    for i in range(len(a)):
         for j in range(len(a)-1, i, -1):
             if a[j] < a[j-1]:
                 (a[j], a[j-1]) = (a[j-1], a[j])
-                counter += 1
-   
-    print('Array is sorted in '+str(counter)+' swaps.')
-    print('First Element: '+str(a[0]))
-    print('Last Element: '+str(a[-1]))
+            print(a)
 
+
+b = [10, 9, 8, 7, 6]
+bubblesort1(b)
 # Move the largest number to the end
+
+print('-----------------')
 
 
 def bubblesort2(a):
-    counter = 0
-    for i in range(len(a)-1):
+    for i in range(len(a)):
+        print(i)
         for j in range(1, len(a)-i):
             if a[j] < a[j-1]:
                 (a[j], a[j-1]) = (a[j-1], a[j])
-                counter += 1
-          
-    print('Array is sorted in '+str(counter)+' swaps.')
-    print('First Element: '+str(a[0]))
-    print('Last Element: '+str(a[-1]))
+            print(a)
+
+
+c = [10, 9, 8, 7, 6]
+bubblesort2(c)
