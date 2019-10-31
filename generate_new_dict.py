6f45847827f427322c61c064e8fd7b708ef64c10
mike <ke265379ke@gmail.com>
2019-10-31 12:06:52 +0800

    Update and rename intersection_dictionary.py to generate_new_dict.py

---
+++ generate_new_dict.py
@@ -0,0 +1,18 @@
+from operator import itemgetter
+
+
+def generate_new_dict(ordered_struct, old_dict):
+    '''
+        if you want change all dict's key or 
+        you only want get some key-value pairs 
+        this function can help
+    '''
+    new_val = itemgetter(*ordered_struct)(old_dict)
+    return dict(zip(ordered_struct, new_val))
+
+
+dt = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+new_key = ('a', 'b', 'c')
+
+new_dt = generate_new_dict(new_key, dt)
+# new_dt = {'c': 3, 'a': 1, 'b': 2}
