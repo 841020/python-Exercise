812b96cd070e1758449d0c3a1b26ca4fcbb4629c
mike <ke265379ke@gmail.com>
2019-11-19 00:30:42 +0800

    Delete selectionSort.py

--- selectionSort.py
+++
@@ -1,9 +0,0 @@
-def selectionSort(args):
-    for i in range(len(args)):
-        min_index = args.index(min(args[i:]))
-        if args[i] > min(args[i:]):
-            args[i], args[min_index] = args[min_index], args[i]
-    print(args)
-    return args
-
-selectionSort([5,4,3,2,1])
