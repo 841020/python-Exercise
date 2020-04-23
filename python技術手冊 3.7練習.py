b8319a634963ad1f2ee8487890e3af6f424b3aa8
mike <ke265379ke@gmail.com>
2020-04-23 09:58:28 +0800

    Update python技術手冊 3.7練習.py

--- python技術手冊 3.7練習.py
+++ python技術手冊 3.7練習.py
@@ -1,73 +1,82 @@
-#第三章練習
-#3.1
+# 第三章練習
+# 3.1
+import xml.etree.ElementTree as ET
+from functools import total_ordering
+import random
 import sys
 str1 = sys.argv[1:]
 print('有{}個不重複字串:{}'.format(len(set(str1)), set(str1)))
-#3.2
+# 3.2
 str2 = sys.argv[1]
 str3 = sys.argv[2:]
 print('{} 出現了{}次'.format(str2, str3.count(str2)))
 
-#第四章練習
+# 第四章練習
 # 4.1
-#armstrong number
+# armstrong number
 armstrong_num = []
-for i in range(100,1000):
-    a=str(i)
+for i in range(100, 1000):
+    a = str(i)
     if int(a[0])**3 + int(a[1])**3 + int(a[2])**3 == i:
         armstrong_num.append(i)
 print(armstrong_num)
-#4.2
-#費波那係數
-def fn(n):
-    a=0
-    b=1
-    c=0
-    for i in range(n):
-        print(c,end=' ')
-        c=a+b
-        a,b=c,a
+# 4.2
+# 費波那係數
 
-n=input()
+
+def fn(n):
+    a = 0
+    b = 1
+    c = 0
+    for i in range(n):
+        print(c, end=' ')
+        c = a+b
+        a, b = c, a
+
+
+n = input()
 fn(int(n))
-#費波那係數 推算
+# 費波那係數 推算
+
+
 def fn(n):
     if n == 1:
         return 0
     if n == 2:
         return 1
 
-    
-    return fn(n-1) +fn(n-2)
-n=input()
+    return fn(n-1) + fn(n-2)
+
+
+n = input()
 print(fn(int(n)))
-#4.3
-#撲克牌洗牌
-color = {'桃','心','梅','磚'}
-number = {'A','2','3','4','5','6','7','8','9','10','J','Q','K'}
+# 4.3
+# 撲克牌洗牌
+color = {'桃', '心', '梅', '磚'}
+number = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'}
 poke = {i+j for i in color for j in number}
 print(poke)
-#4.4
-a=[(i,j,k) for i in range(1, 11) for j in range(1,11) for k in range(1,11) if (i**2)+(j**2)==k**2 and (i+j+k)== 24]
+# 4.4
+a = [(i, j, k) for i in range(1, 11) for j in range(1, 11)
+     for k in range(1, 11) if (i**2)+(j**2) == k**2 and (i+j+k) == 24]
 print(a)
 
-#第六章
-#6.1
-import random
+# 第六章
+# 6.1
 
-num=str(random.randint(0,9))
-n=input('guess a number')
-while n!=num:
-  n=input('guess a number')
+num = str(random.randint(0, 9))
+n = input('guess a number')
+while n != num:
+    n = input('guess a number')
 print('猜中了')
 
-#6.2
-#Implement rich comparison
-from functools import total_ordering
+# 6.2
+# Implement rich comparison
+
 
 @total_ordering
 class Rational:
-    def __init__(self, numer:int, denom: int) -> None:
+    def __init__(self, numer: int, denom: int) -> None:
         self.numer = numer
         self.denom = denom
 
@@ -98,45 +107,55 @@ class Rational:
     def __str__(self):
         return f'{self.numer}/{self.denom}'
 
-    def __repr__(self): 
+    def __repr__(self):
         return f'Rational({self.numer}, {self.denom})'
+
     def __eq__(self, that):
-      return self.numer/self.denom == that.numer/that.denom
+        return self.numer/self.denom == that.numer/that.denom
+
     def __gt__(self, that):
-      return self.numer/self.denom > that.numer/that.denom
-s1=Rational(2,5)
-s2=Rational(1,5)
+        return self.numer/self.denom > that.numer/that.denom
 
-print(s1>s2)
-print(s1>=s2)
-print(s2<s1)
-print(s2<=s1)
-print(s1==s2)
-print(s1!=s2)
 
-#第七章練習
+s1 = Rational(2, 5)
+s2 = Rational(1, 5)
 
-#7.1
-#customize exception
+print(s1 > s2)
+print(s1 >= s2)
+print(s2 < s1)
+print(s2 <= s1)
+print(s1 == s2)
+print(s1 != s2)
+
+# 第七章練習
+
+# 7.1
+# customize exception
+
+
 class BankException(Exception):
-  def __init__(self, message):
-    super().__init__(message)
+    def __init__(self, message):
+        super().__init__(message)
+
 
 class IllegalMoneyException(BankException):
-  def __init__(self, message):
-    super().__init__(message)
+    def __init__(self, message):
+        super().__init__(message)
+
 
 class InsufficientException(BankException):
-  def __init__(self, message):
-    super().__init__(message)
+    def __init__(self, message):
+        super().__init__(message)
+
+
 class Account:
     def __init__(self, name: str, number: str, balance: float) -> None:
         self.name = name
         self.number = number
         self.balance = balance
-    
+
     def amount_check(self, amount: float):
-      if amount <= 0:
+        if amount <= 0:
             raise IllegalMoneyException(f'存款金額不得為負{amount}')
 
     def deposit(self, amount: float):
@@ -153,44 +172,52 @@ class Account:
     def __str__(self):
         return f"Account('{self.name}', '{self.number}', {self.balance})"
 
-a=Account('mike', '123',1000)
+
+a = Account('mike', '123', 1000)
 a.withdraw(2000)
 a.withdraw(-2000)
 a.deposit(-100)
 
-#7.2
-#Implement context manager
+# 7.2
+# Implement context manager
+
+
 class Suppress:
-  def __init__(self, ex_type):
-    self.ex_type = ex_type
-  def __enter__(self):
-    return self
-  def __exit__(self, exc_type,exc_value, traceback):
-    if exc_type is self.ex_type:
-      return True
-    return False
+    def __init__(self, ex_type):
+        self.ex_type = ex_type
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is self.ex_type:
+            return True
+        return False
+
 
 with Suppress(ZeroDivisionError):
-  a=9/0
+    a = 9/0
 
-#第九章練習
+# 第九章練習
 
-#9.2
+# 9.2
 words = ['RADAR', 'WARTER START', 'MILK KLIM', 'RESERVERED', 'IWI', 'ABBA']
 output = []
 for c in words:
-  if c == c[::-1]:
-    output.append(c)
-    
-#第十章
-#10.2
-import xml.etree.ElementTree as ET
+    if c == c[::-1]:
+        output.append(c)
+
+# 第十章
+# 10.2
+
 
 def dict_to_xml(root, kwargs):
-  user = ET.Element(root)
-  for key, value in kwargs.items():
-    key = ET.Element(key)
-    key.text = str(value)
-    user.append(key)
-  return ET.tostring(user).decode()
-dict_to_xml('user', {'age':40, 'name':'Justin'})
+    user = ET.Element(root)
+    for key, value in kwargs.items():
+        key = ET.Element(key)
+        key.text = str(value)
+        user.append(key)
+    return ET.tostring(user).decode()
+
+
+dict_to_xml('user', {'age': 40, 'name': 'Justin'})
